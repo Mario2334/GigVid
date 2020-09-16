eebdee141a82799fb9bccbd431e6880fc45d8c7f
sanket mokashi <sanketm221995@gmail.com>
2020-09-16 19:31:42 +0530

    Removed Node Project. Added zappa for serverless deployement

--- gigapi/controllers/indexer.js
+++
@@ -1,6 +0,0 @@
-let db = require('../models');
-
-exports.test = async () => {
-    let user = await db.User.findAll()
-    return user
-}
--- gigapi/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const user_controller = require('../controllers/user_controller');
-
-/* GET users listing. */
-router.post('/create_user', user_controller.create_user);
-router.post('/login',user_controller.login)
-
-module.exports = router;
