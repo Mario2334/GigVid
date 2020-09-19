861b952116e76fdc5a9a9ad13cf2fde680944953
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2020-09-19 13:47:41 +0530

    Implementing search bar onClick of search icon

--- gigvid_app/app/src/main/java/com/android/gigvid/view/homescreen/HomeScreenActivity.java
+++ gigvid_app/app/src/main/java/com/android/gigvid/view/homescreen/HomeScreenActivity.java
@@ -1,26 +1,85 @@
 package com.android.gigvid.view.homescreen;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import com.android.gigvid.R;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class HomeScreenActivity extends AppCompatActivity {
+    private BottomNavigationView navView;
+    private MaterialToolbar topAppBar;
+    private MenuItem searchMenuItem;
+    private SearchView searchView;
+    private View activityView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        initializeUI();
+    }
+
+    /**
+     * Method: Initialize UI
+     */
+    private void initializeUI() {
+        activityView = findViewById(android.R.id.content);
+        navView = findViewById(R.id.nav_view);
+        topAppBar = findViewById(R.id.top_menu_bar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    /**
+     * Method: Launch Home Fragment when home button is clicked
+     */
+    private void handleNavigationIconClick() {
+        topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Launch Home Fragment
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+
+        searchMenuItem = menu.findItem(R.id.search);
+        searchView = (SearchView) searchMenuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                // Show message on Snack-bar
+                Snackbar.make(activityView, "Searching: " + query, Snackbar.LENGTH_SHORT).show();
+
+                searchMenuItem.collapseActionView();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return true;
+    }
 }
