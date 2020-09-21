fd985ac3e92f37cdde616567a1a733f732c2c6eb
spophali <spophali@gmail.com>
2020-09-21 20:51:02 +0530

    My Gig adapter impl

---
+++ gigvid_app/app/src/main/java/com/android/gigvid/view/homescreen/adapter/MyGigAdapter.java
@@ -0,0 +1,112 @@
+package com.android.gigvid.view.homescreen.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.gigvid.R;
+import com.android.gigvid.model.repository.networkRepo.homeScreen.pojo.GigListResp;
+import com.android.gigvid.model.repository.networkRepo.homeScreen.pojo.ScheduleDateTime;
+import com.android.gigvid.utils.dateTime.DateTimeUtils;
+
+import java.util.List;
+
+public class MyGigAdapter extends RecyclerView.Adapter<MyGigAdapter.MyGigAdapterVH> {
+
+    private List<GigListResp> gigList;
+    @NonNull
+    @Override
+    public MyGigAdapterVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.gig_item_my_gigs, parent , false);
+        return new MyGigAdapterVH(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyGigAdapterVH holder, int position) {
+        GigListResp myGigData = gigList.get(position);
+        ScheduleDateTime scheduleDateTime = DateTimeUtils.getSchedDtTime(myGigData.getScheduledTime());
+        holder.getGigTitle().setText(myGigData.getName());
+        holder.getGigDescrip().setText(myGigData.getDescription());
+
+//        holder.getGigPrice().setText(String.valueOf(myGigData.getPrice()));
+        holder.getGigDate().setText(scheduleDateTime.getDate());
+        holder.getGigTime().setText(scheduleDateTime.getTime());
+        holder.getGigMonth().setText(scheduleDateTime.getMonth());
+        holder.getGigDuration().setText(myGigData.getDuration().toString()+"hr(s)");
+
+//        holder.getGigBuyBtn().setTag(Constants.BUY_BTN_TAG_KEY, gigListResp.getId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return gigList != null ? gigList.size() : 0;
+    }
+
+    public void setData(List<GigListResp> data) {
+        gigList = data;
+    }
+
+    public class MyGigAdapterVH extends RecyclerView.ViewHolder {
+
+        private TextView gigDate;
+        private TextView gigMonth;
+        private TextView gigTitle;
+        private TextView gigPrice;
+        private TextView gigDescrip;
+        private TextView gigDuration;
+
+        public TextView getGigDate() {
+            return gigDate;
+        }
+
+        public TextView getGigMonth() {
+            return gigMonth;
+        }
+
+        public TextView getGigTitle() {
+            return gigTitle;
+        }
+
+        public TextView getGigPrice() {
+            return gigPrice;
+        }
+
+        public TextView getGigDescrip() {
+            return gigDescrip;
+        }
+
+        public TextView getGigDuration() {
+            return gigDuration;
+        }
+
+        public TextView getGigTime() {
+            return gigTime;
+        }
+
+        public Button getStartGigBtn() {
+            return startGigBtn;
+        }
+
+        private TextView gigTime;
+        private Button startGigBtn;
+
+
+        public MyGigAdapterVH(@NonNull View itemView) {
+            super(itemView);
+
+            gigDate = (TextView) itemView.findViewById(R.id.my_gigs_date);
+            gigMonth = (TextView) itemView.findViewById(R.id.my_gigs_month);
+            gigTitle = (TextView) itemView.findViewById(R.id.my_gigs_title);
+//            gigPrice = (TextView) itemView.findViewById(R.id.gig_price);
+            gigDescrip = (TextView) itemView.findViewById(R.id.my_gigs_description);
+            gigTime = (TextView) itemView.findViewById(R.id.my_gigs_time);
+            gigDuration = (TextView) itemView.findViewById(R.id.my_gigs_duration);
+            startGigBtn = (Button) itemView.findViewById(R.id.start_event_button);
+        }
+    }
+}
--- gigvid_app/app/src/main/java/com/android/gigvid/view/homescreen/fragment/MyGigFragment.java
+++ gigvid_app/app/src/main/java/com/android/gigvid/view/homescreen/fragment/MyGigFragment.java
@@ -6,29 +6,71 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.gigvid.GigVidApplication;
 import com.android.gigvid.R;
+import com.android.gigvid.model.repository.networkRepo.homeScreen.pojo.GigListResp;
+import com.android.gigvid.model.repository.reponseData.ListResponse;
+import com.android.gigvid.model.repository.reponseData.StateDefinition;
+import com.android.gigvid.view.homescreen.adapter.GigListAdapter;
+import com.android.gigvid.view.homescreen.adapter.MyGigAdapter;
 import com.android.gigvid.viewModel.homescreen.MyGigViewModel;
+
+import timber.log.Timber;
 
 public class MyGigFragment extends Fragment {
 
     private MyGigViewModel myGigViewModel;
+    private RecyclerView mMyGigsRecyclerView;
+    private MyGigAdapter mMyGigAdapter;
+    private LinearLayoutManager mLayoutManager;
+
+    private Observer<ListResponse<GigListResp>> mMyGigRespStatusObserver = new Observer<ListResponse<GigListResp>>() {
+        @Override
+        public void onChanged(ListResponse<GigListResp> gigListRespStatus) {
+            if (gigListRespStatus.getStatus() == StateDefinition.State.COMPLETED) {
+                Timber.d("my gig success %d", gigListRespStatus.getData().size());
+
+                mMyGigAdapter.setData(gigListRespStatus.getData());
+                mMyGigAdapter.notifyDataSetChanged();
+            } else {
+                Timber.d("my gig api failed");
+            }
+        }
+    };
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         myGigViewModel =
                 ViewModelProviders.of(this).get(MyGigViewModel.class);
         View root = inflater.inflate(R.layout.fragment_mygig, container, false);
-        myGigViewModel.getMyGigList();
-//        final TextView textView = root.findViewById(R.id.text_mygig);
-//        myGigViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        myGigViewModel.getMyGigList().observe(this,mMyGigRespStatusObserver);
+
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        mMyGigsRecyclerView = view.findViewById(R.id.my_gig_recycler_view);
+        setUpRecyclerViewAdapter();
+    }
+
+    private void setUpRecyclerViewAdapter() {
+        mLayoutManager = new LinearLayoutManager(GigVidApplication.getGigVidAppContext());
+        mLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        mMyGigsRecyclerView.setLayoutManager(mLayoutManager);
+        mMyGigsRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        if (mMyGigAdapter == null) {
+            mMyGigAdapter = new MyGigAdapter();
+        }
+        mMyGigsRecyclerView.setAdapter(mMyGigAdapter);
     }
 }
