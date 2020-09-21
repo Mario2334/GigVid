995257069c349a7d76ccc22d8225caa382120076
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2020-09-21 21:06:10 +0530

    Implementing Progress Bar for home screen

--- gigvid_app/app/src/main/java/com/android/gigvid/view/homescreen/fragment/HomeFragment.java
+++ gigvid_app/app/src/main/java/com/android/gigvid/view/homescreen/fragment/HomeFragment.java
@@ -7,10 +7,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -18,6 +20,7 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.android.gigvid.GigVidApplication;
 import com.android.gigvid.R;
 import com.android.gigvid.model.repository.networkRepo.homeScreen.pojo.buygig.BuyGigReqBody;
@@ -34,21 +37,40 @@ import timber.log.Timber;
 
 public class HomeFragment extends Fragment implements AdapterEventCommunicator {
 
+    private String LOADING_ANIMATION = "progress_bar.json";
+    private String ERROR_ANIMATION = "error.json";
+
     private HomeViewModel homeViewModel;
     private RecyclerView listGigsRecyclerView;
     private GigListAdapter gigListAdapter;
     private AdapterEventCommunicator mAdapterEventCommunicator;
+    private ConstraintLayout progressBarLayoutView;
+    private Button retryButton;
+    private LottieAnimationView progressBarLottieView;
+    String loadingAnimation;
 
     private Observer<ListResponse<GigListResp>> gigListRespStatusObserver = new Observer<ListResponse<GigListResp>>() {
         @Override
         public void onChanged(ListResponse<GigListResp> gigListRespStatus) {
             if (gigListRespStatus.getStatus() == StateDefinition.State.COMPLETED) {
                 Timber.d("list gig success %d", gigListRespStatus.getData().size());
-
+                progressBarLayoutView.setVisibility(View.GONE);
                 gigListAdapter.setData(gigListRespStatus.getData());
                 gigListAdapter.notifyDataSetChanged();
+            } else if (gigListRespStatus.getStatus() == StateDefinition.State.ERROR) {
+                if (progressBarLayoutView.getVisibility() != View.VISIBLE) {
+                    progressBarLayoutView.setVisibility(View.VISIBLE);
+                }
+                loadingAnimation = ERROR_ANIMATION;
+                loadLottieAnimations(loadingAnimation);
+                retryButton.setVisibility(View.VISIBLE);
+                handleRetryButtonClick();
             } else {
-                Timber.d("list gig api failed");
+                Timber.d("list gig api LOADING");
+                progressBarLayoutView.setVisibility(View.VISIBLE);
+                retryButton.setVisibility(View.GONE);
+                loadingAnimation = LOADING_ANIMATION;
+                loadLottieAnimations(loadingAnimation);
             }
         }
     };
@@ -70,6 +92,9 @@ public class HomeFragment extends Fragment implements AdapterEventCommunicator {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        progressBarLayoutView = view.findViewById(R.id.progress_bar_view);
+        progressBarLottieView = view.findViewById(R.id.display_current_progress);
+        retryButton = view.findViewById(R.id.retry_button);
         listGigsRecyclerView = view.findViewById(R.id.list_gigs_recycler_view);
 
         setUpRecyclerViewAdapter();
@@ -105,16 +130,52 @@ public class HomeFragment extends Fragment implements AdapterEventCommunicator {
             if (buyGigRespDataResponse.getStatus() == StateDefinition.State.COMPLETED) {
                 Log.d("SMP", "onChanged: buy gig on change observer" + buyGigRespDataResponse.getData().getLink());
 
-
+                progressBarLayoutView.setVisibility(View.GONE);
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(buyGigRespDataResponse.getData().getLink()));
                 startActivity(browserIntent);
             } else if (buyGigRespDataResponse.getStatus() == StateDefinition.State.ERROR) {
                 Log.d("SMP", "onChanged: error");
+                if (progressBarLayoutView.getVisibility() != View.VISIBLE) {
+                    progressBarLayoutView.setVisibility(View.VISIBLE);
+                }
+                loadingAnimation = ERROR_ANIMATION;
+                loadLottieAnimations(loadingAnimation);
+                retryButton.setVisibility(View.VISIBLE);
+                handleRetryButtonClick();
                 Toast.makeText(GigVidApplication.getGigVidAppContext(), buyGigRespDataResponse.getError().getErrorMsg(), Toast.LENGTH_SHORT).show();
             } else {
-//                Handle loading screen
+                progressBarLayoutView.setVisibility(View.VISIBLE);
+                retryButton.setVisibility(View.GONE);
+                loadingAnimation = LOADING_ANIMATION;
+                loadLottieAnimations(loadingAnimation);
             }
 
         }
     };
+
+    /**
+     * Method: Load Lottie Animation View to display progress
+     */
+    private void loadLottieAnimations(String animationName) {
+        if(progressBarLottieView.isAnimating()) {
+            progressBarLottieView.cancelAnimation();
+        }
+        progressBarLottieView.setAnimation(animationName);
+        progressBarLottieView.loop(true);
+        progressBarLottieView.playAnimation();
+    }
+
+    /**
+     * Method: Retry Button
+     *          1. Hides progress UI
+     *          2. TODO: Will reconnect to network
+     */
+    private void handleRetryButtonClick() {
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBarLayoutView.setVisibility(View.GONE);
+            }
+        });
+    }
 }
